1a91bef7b29e12eb144615f5f26e438d05fdfd9e
Khanija-Git <medakakhanija28@gmail.com>
2022-09-03 11:04:43 +0530

    This program is about the variables.

---
+++ Variable/src/hello/world/Variable.java
@@ -0,0 +1,17 @@
+package hello.world;
+
+public class Variable 
+{
+	public void StudentAge()
+	{
+		int age = 0;
+		age = age+20;
+		System.out.println("Student age is:"+age);
+	}
+ 
+	public static void main(String[] args) {
+		Variable temp = new Variable();
+		temp.StudentAge();
+		
+	}
+}
